23886d95a1d67908af7b9cb94f742588a8fc74ed
Mochi <tianqifeicanghao@gmail.com>
2021-01-30 10:02:51 -0500

    installed firebase and added config

---
+++ src/Firebase/firebase.tsx
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyDSWRZGHKWVmMcGIIQQm2L1t-z0tvB8LPM",
+    authDomain: "swamphacks2021-poki.firebaseapp.com",
+    projectId: "swamphacks2021-poki",
+    storageBucket: "swamphacks2021-poki.appspot.com",
+    messagingSenderId: "193865770028",
+    appId: "1:193865770028:web:19e56fda870722bd681d89",
+    measurementId: "G-PGFB8FWL08"
+  };
+firebase.initializeApp(firebaseConfig);
+export default firebase;
