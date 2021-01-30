c9f93f403d30e194a08192364de5c797f6e47a5d
Mochi <tianqifeicanghao@gmail.com>
2021-01-30 11:45:34 -0500

    user service

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Login from './Auth/login'
 import Home from './Home/home'
 import { FirebaseAppProvider } from 'reactfire';
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyDSWRZGHKWVmMcGIIQQm2L1t-z0tvB8LPM",
   authDomain: "swamphacks2021-poki.firebaseapp.com",
   projectId: "swamphacks2021-poki",
   storageBucket: "swamphacks2021-poki.appspot.com",
+  databaseURL: "https://swamphacks2021-poki-default-rtdb.firebaseio.com",
   messagingSenderId: "193865770028",
   appId: "1:193865770028:web:19e56fda870722bd681d89",
   measurementId: "G-PGFB8FWL08"
--- src/Home/home.tsx
+++ src/Home/home.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+// import { setUser } from '../Shared/user.service'
 import './home.css'
-
+import {GetUser, setUser} from '../Shared/user.service'
+// import firebase from 'firebase/app'
+// import {useFirestore} from 'reactfire'
 function Home() {
+  // const user= useFirestore().collection('users').doc('testuser')
+  // console.log(user)
+  const {status , data } = GetUser("testuser")
+  console.log(status)
+  console.log(data)
   return (
     <div>
     Home!
