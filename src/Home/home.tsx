5fb85c1bd8e3e265482da127946403d446f8f43e
angelaxie2015 <angelaxie2015@gmail.com>
2021-01-30 12:08:20 -0500

    added navbar

--- src/Home/home.tsx
+++ src/Home/home.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './home.css'
-// import '../Navbar/navigation.tsx'
+import Navigation from '../Navigation/navigation'
 
 function Home() {
   return (
     <div id="home-page">
-	    <h1>Gator-Mon!</h1>
-	    <img src="../../gator_cartoon.png" alt="gator_cartoon"></img>
+    	<Navigation />
+
+	    <div className="row">
+	    	<div className="col">
+	    		<h1 className="home-name">Gator-Mon!</h1>
+		    	<h2 className="home-descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+		    </div>
+		    <div className='col'>
+		    	<img className="home-img" src="../../gator_cartoon.png" alt="gator_cartoon"></img>
+		    </div>
+	    </div>
     </div>
 
   )
