41738358e776c26c240603617ec52e2b9cd118fc
Leon <kwan.leon@gmail.com>
2021-01-30 16:15:31 -0500

    merge

--- src/Home/home.tsx
+++ src/Home/home.tsx
@@ -5,10 +5,6 @@ import Navigation from '../Navigation/navigation'
 import {GetUser, setUser} from '../Shared/user.service'
 //import firebase from 'firebase/app'
 //import {useFirestore} from 'reactfire'
-import GameMap from '../Map/map'
-
-const test_players = {id:0, location: {lat: 29.65, lng: -82.3}}
-const test_other_players = [{id:1, location: {lat: 29.7, lng: -82.3}}, {id: 2, location: {lat: 29.6, lng: -82.3}}]
 
 function Home() {
   // const user= useFirestore().collection('users').doc('testuser')
@@ -29,9 +25,6 @@ function Home() {
 		    	<img className="home-img" src="../../gator_cartoon.png" alt="gator_cartoon"></img>
 		    </div>
 	    </div>
-      <div className="GameMap">
-        <GameMap center={{lat: 29.65, lng: -82.3}} player={test_players} mons = {[]} other_players={test_other_players}/>
-      </div>
     </div>
 
   )
