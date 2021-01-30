97fcb9267da7f6b2f05625a46f80718aefa7b963
Tianrui Li <littealeaf.2001@gmail.com>
2021-01-29 21:11:33 -0800

    Updates css and React fire config

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,28 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './Auth/login'
 import Home from './Home/home'
+import { FirebaseAppProvider } from 'reactfire';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSWRZGHKWVmMcGIIQQm2L1t-z0tvB8LPM",
+  authDomain: "swamphacks2021-poki.firebaseapp.com",
+  projectId: "swamphacks2021-poki",
+  storageBucket: "swamphacks2021-poki.appspot.com",
+  messagingSenderId: "193865770028",
+  appId: "1:193865770028:web:19e56fda870722bd681d89",
+  measurementId: "G-PGFB8FWL08"
+};
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/login' component={Login}/>
-        <Route path='/' component={Home}/>
-      </Switch>
-    </BrowserRouter>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login}/>
+          <Route path='/' component={Home}/>
+        </Switch>
+      </BrowserRouter>
+    </FirebaseAppProvider>
   );
 }
 
