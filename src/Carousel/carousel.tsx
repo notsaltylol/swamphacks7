d43ba27e13f2b55bcefec467e19eaffcd99cb972
Leon <kwan.leon@gmail.com>
2021-01-31 01:53:14 -0500

    carousel

--- src/Carousel/carousel.tsx
+++ src/Carousel/carousel.tsx
@@ -1,56 +1,56 @@
 import React from 'react'
 import Navigation from '../Navigation/navigation'
 import './carousel.css'
-import Carousel, { Dots, slidesToShowPlugin } from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
+import {Carousel} from 'react-bootstrap'
 import {useState} from 'react'
+
+const test_caught = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function CarouselComponent(){
-    const [picCount, setPicCount] = useState(0);
-    const onChange =(val: number)=>{
+    const [showAll, setShowAll] = useState(true);
+    const [picCount, setPicCount] = useState(20);
+    const onChange = (val: number)=>{
         setPicCount(val);
     }
+    const [index, setIndex] = useState(0);
+    //const handleSelect = (selectedIndex, e) => {
+    //    setIndex(selectedIndex);
+    //};
     // const [slides, setSlides] = useState({
     //     [
     //         (<img src="../../public/gator1.png"/>)
     //     ]
     // })
     return(
-        
-        <div>
+        <div className="carousel-container">
             <Navigation />
-            Carousel is heree
-            <Carousel
-                value={picCount}
-                onChange={onChange}
-                slides={[
-                    (<img src='./gator3.png'></img>),
-                    (<img src='./gator3.png'></img>),
-                    // (<img src={imageTwo} />),
-                    // (<img src={imageThree} />),
-                  ]}
-                  plugins={[
-                    'arrows',
-                    'clickToChange'
-                  ]}
-                  />
-
-
-              <Carousel
-                    plugins={[
-                        'clickToChange',
-                        'centered',
-                        {
-                        resolve: slidesToShowPlugin,
-                        options: {
-                        numberOfSlides: 2
-                        }
-                        },
-                    ]}
-                    >
-                    <img src='./gator3.png'></img>
-                    <img src='./gator3.png'></img>
-                    <img src='./gator3.png'></img>
-        </Carousel>     
+            {showAll ?
+                <Carousel>
+                {
+                    [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19].map((i) => {
+                        return(<Carousel.Item key={i}>
+                            <img className="d-block w-50 cimage" 
+                            src={"gator" + (i+1).toString() + ".png"}
+                            />
+                            {console.log(i)}
+                        </Carousel.Item>
+                        )
+                    })
+                }
+                </Carousel>
+               : picCount === 0?
+                <Carousel>
+                    <div></div>
+                    {
+                        test_caught.map((i) => {
+                            return(<Carousel.Item key={i}>
+                                <img className="d-block w-100 cimage" src={"gator" + (i+1).toString() + ".png"}/>
+                            </Carousel.Item>
+                            )
+                        })
+                    }
+                </Carousel> : <h1>Come back after you catch some Gatormons!</h1>
+                
+            }
         </div>
     )
 }
